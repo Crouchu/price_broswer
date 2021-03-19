51f2cb8a7c7af9f7e637a8395606f1089bcecb10
Piotr <piotrwcislo26@gmail.com>
2021-03-19 17:14:46 +0100

    added user app

---
+++ user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from user import views
+
+app_name ='user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view()),
+    path('token/', views.CreateTokenView.as_view()),
+    path('me/', views.ManageUserView.as_view()),
+]
